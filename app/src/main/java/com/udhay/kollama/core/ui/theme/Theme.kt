591fcd6a05a4b3e4b66b369d1df394f96fa1354f
Udhay-Adithya <udhayxd@gmail.com>
2026-04-16 14:27:40 +0530

    refactor: rename theme package and update color scheme implementation

---
+++ app/src/main/java/com/udhay/kollama/core/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.udhay.kollama.core.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.materialkolor.dynamiccolor.ColorSpec
+import com.materialkolor.rememberDynamicColorScheme
+
+@Composable
+fun KollamaTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = false,
+    isAmoled: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val generatedColorScheme =
+        rememberDynamicColorScheme(
+            seedColor = seedColor,
+            isDark = darkTheme,
+            isAmoled = isAmoled,
+            specVersion = ColorSpec.SpecVersion.SPEC_2025,
+        )
+
+
+    MaterialTheme(
+        colorScheme = generatedColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
--- app/src/main/java/com/udhay/kollama/core/ui/theme/Type.kt
+++ app/src/main/java/com/udhay/kollama/core/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.udhay.kollama.ui.theme
+package com.udhay.kollama.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
