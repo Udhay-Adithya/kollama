216a6ace54a811116b022239ca6fa5622e7b2f0e
Udhay-Adithya <udhayxd@gmail.com>
2026-04-26 22:32:28 +0530

    feat: initialize app database

---
+++ app/src/main/java/com/udhay/kollama/core/database/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.udhay.kollama.core.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.udhay.kollama.feature.settings.data.local.UserSettingsEntity
+import com.udhay.kollama.feature.settings.data.local.UserSettingsDao
+
+@Database(
+    entities = [
+        UserSettingsEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userSettingsDao(): UserSettingsDao
+
+    companion object {
+        fun create(context: Context): AppDatabase =
+            Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "kollama.db"
+            )
+                .build()
+    }
+}
